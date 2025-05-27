55e1013497563132c12e4be85f17eac74344bd05
skywalker <lipeeskywalker@gmail.com>
2025-05-27 11:39:05 -0300

    fix: now loading the page after animation

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,8 @@
 window.addEventListener("load", function () {
     const loader = document.getElementById("loader-wrapper");
+
+    document.body.classList.add("loaded");
+
     loader.classList.add("hide");
     setTimeout(() => loader.style.display = "none", 500); // espera a transição
 });
@@ -8,4 +11,5 @@ function showLoader() {
     const loader = document.getElementById("loader-wrapper");
     loader.classList.remove("hide");
     loader.style.display = "flex";
+    document.body.classList.remove("loaded");
 }
